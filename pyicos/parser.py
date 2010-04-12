ca0c2399cae22a505a443986de7f789cc4488c21
Juan González-Vallinas <juanramon.gonzalezvallinas@upf.edu>
2010-04-12 12:17:50 +0000

    Trim operation added

--- pyicos/parser.py
+++ pyicos/parser.py
@@ -1,7 +1,7 @@
 import sys
 
 from pyicos.lib import argparse
-from pyicos.operations import (Turbomix, Extend, Poisson, RemoveRegion, RemoveChromosome, Normalize, Subtract,
+from pyicos.operations import (Turbomix, Extend, Poisson, RemoveRegion, RemoveChromosome, Normalize, Subtract, Trim,
                         Split, Cut, NoWrite, DiscardArtifacts, RemoveDuplicates, OperationFailed, ModFDR)
 from pyicos.core import (BED, ELAND, PK, SPK, ELAND_EXPORT, WIG, CLUSTER_FORMATS, READ_FORMATS, WRITE_FORMATS)
 __version__ = '0.8.1'
@@ -52,7 +52,7 @@ class PicosParser:
         label.add_argument('--label', default='pyicos_output', help='The label that will identify the experiment')
 
         span = self.new_subparser()
-        span.add_argument('--span', default=25, help='The span of the variable and fixed wig formats')
+        span.add_argument('--span', default=25, help='The span of the variable and fixed wig formats', type=int)
 
         round = self.new_subparser()
         round.add_argument('--round',action='store_true',dest='rounding', default=False, help='Round the final results to an integer')
@@ -83,13 +83,17 @@ class PicosParser:
         normalize.add_argument('--normalize',action='store_true', default=False, help='Normalize to the control before subtracting')
 
         trim_percentage = self.new_subparser()
-        trim_percentage.add_argument('--trim', default=0.05, help='Fraction of the cluster height below which the peak is trimmed. Example: For a cluster of height 40, if the flag is 0.05, 40*0.05=2. Every cluster will be trimmed to that height. A position of height 1 is always considered insignificant, no matter what the cluster height is. [Default %(default)s]')
+        trim_percentage.add_argument('--trim-per', default=0.05, help='Fraction of the cluster height below which the peak is trimmed. Example: For a cluster of height 40, if the flag is 0.05, 40*0.05=2. Every cluster will be trimmed to that height. A position of height 1 is always considered insignificant, no matter what the cluster height is. [Default %(default)s]', type=float)
+        trim_absolute = self.new_subparser()
+        trim_absolute.add_argument('--trim-abs', help='The height threshold used to split or trim the clusters.', type=int)
 
         discard = self.new_subparser()
         discard.add_argument('--discard', help='Discard the reads that have this particular tag. Example: --discard chr1 will discard all reads with chr1 as tag. You can specify multiple tags to discard using the following notation --discard chr1:chr2:tagN')
 
         threshold = self.new_subparser()
         threshold.add_argument('--threshold', help='The height threshold used to cut', type=int)
+
+
         #callpeaks operation
         subparsers.add_parser('callpeaks', help='The complete peak calling sequence proposed in the future publication. The region file is optional. The same goes for the control file, if not provided, there will not be a normalization or a subtraction.', parents=[parserinput, optional_control, control_format, open_control, optional_region, output, output_flags, frag_size, round, label, span, no_subtract, discard, pvalue, height, correction, trim_percentage])
         #convert operation
@@ -100,7 +104,9 @@ class PicosParser:
         #subtract operation
         subparsers.add_parser('subtract', help='Subtract two pk files. Operating with directories will only give apropiate results if the files and the control are paired in alphabetical order.', parents=[parserinput, control, control_format, open_control, output, output_flags, round, normalize, tag_length, span, label])
         #split operation
-        subparsers.add_parser('split', help='Split the peaks in subpeaks. Only accepts pk or wig as output (other formats under development).', parents=[parserinput, output, output_flags, round, span, label])
+        subparsers.add_parser('split', help='Split the peaks in subpeaks. Only accepts pk or wig as output (other formats under development).', parents=[parserinput, output, output_flags, round, trim_percentage, trim_absolute, label])
+        #trim operation
+        subparsers.add_parser('trim', help='Trim the clusters to a given threshold.', parents=[parserinput, output, output_flags, round, trim_absolute, label])
         #discard operation
         subparsers.add_parser('discard', help='Discards artifacts from a file. Only accepts pk or wig as output.', parents=[parserinput, output, output_flags, round, span, label])
         #remove duplicates operation
@@ -120,20 +126,18 @@ class PicosParser:
         #remove operation
         subparsers.add_parser('remove', help='Removes regions that overlap with another the coordinates in the "black list" file.',
                               parents=[parserinput, output_flags, region, region_format, output])
-
+        
         parser.set_defaults(discard = None, output=None, control=None, label = 'noname', output_format=PK, open_output =False,  rounding = False,
                             control_format=None, region=None, region_format = BED, open_region = False,
-                            frag_size = None, tag_length = None, span=40, p_value=0.01, height_limit=100, correction=1, no_subtract = False, normalize = False, trim=0.05,
-                            open_control=False, no_sort=False, duplicates=4, threshold=None)
+                            frag_size = None, tag_length = None, span=40, p_value=0.01, height_limit=100, correction=1, no_subtract = False, normalize = False,                      trim_per=0.05,open_control=False, no_sort=False, duplicates=4, threshold=None, trim_abs=7)
         args = parser.parse_args()
-
         if not args.control_format: #If not specified, the control format is equal to the input format
             args.control_format = args.input_format
-
+        
         turbomix = Turbomix(args.input, args.output, args.input_format, args.output_format, args.label, args.open_input, args.open_output, args.debug,
                             args.rounding, args.tag_length, args.discard, args.control, args.control_format, args.open_control, args.region,
-                            args.region_format, args.open_region, args.span, args.frag_size, args.p_value, args.height_limit, args.correction, args.trim,
-                            args.no_sort, args.duplicates, args.threshold)
+                            args.region_format, args.open_region, args.span, args.frag_size, args.p_value, args.height_limit, args.correction,
+                            args.trim_per, args.no_sort, args.duplicates, args.threshold, args.trim_abs)
 
         #if sys.argv[1] == 'convert': No need for this.
         if sys.argv[1] == 'subtract':
@@ -161,6 +165,9 @@ class PicosParser:
 
         elif sys.argv[1] == 'split':
             turbomix.operations = [Split]
+
+        elif sys.argv[1] == 'trim':
+            turbomix.operations = [Trim]
 
         elif sys.argv[1] == 'discard':
             turbomix.operations = [DiscardArtifacts]
