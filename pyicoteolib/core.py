dbec10ffb11e43bfe0203fc02297f05e4bc9e19b
juanrg <juanramon.gonzalezvallinas@upf.edu>
2013-04-11 22:36:17 +0200

    rebranding

--- pyicoteolib/core.py
+++ pyicoteolib/core.py
@@ -1,5 +1,5 @@
 """
-Pyicos is free software: you can redistribute it and/or modify
+Pyicoteo is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
