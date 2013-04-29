0322b2b10ef243ae2f31eb9ca7b134da6c0b5104
Ferran Lloret <ferranlloret@gmail.com>
2013-04-29 10:46:24 +0200

    Fixed pyicoregion arguments.

--- pyicoteolib/parser/pyicoregion.py
+++ pyicoteolib/parser/pyicoregion.py
@@ -4,9 +4,9 @@ import argparse
 
 def create_parser():
     region_parser = new_subparser()
-    region_parser.add_argument('gff-file', help="GFF input file")
+    region_parser.add_argument('--gff-file', help="GFF input file")
     output = new_subparser()
-    output.add_argument('output', help='The output file')
+    output.add_argument('-output', help='The output file')
     region_parser.add_argument('--region-magic', nargs='+', help="Desired features to filter (exons, introns, sliding window for inter-/intragenic zones)")
 
 
