dd9b071327320395d9e30d325c885b70f7c08705
juanrg <juanramon.gonzalezvallinas@upf.edu>
2014-02-19 17:42:50 +0100

    New version with remregions working

--- pyicoteolib/defaults.py
+++ pyicoteolib/defaults.py
@@ -11,8 +11,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-VERSION = "2.0.6b"
+"""
+VERSION = "2.0.7"
 
 #Formats
 ELAND = 'eland'
